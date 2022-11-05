3b54ff5e3561f971498dd3f433bd53a5447412f0
SergioStasevich <kangurek1991@gmail.com>
2022-11-05 17:29:45 +0100

    Initial Commit

---
+++ userTestDefauls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  userTestDefauls
+//
+//  Created by Siarhei Stasevich on 05/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
